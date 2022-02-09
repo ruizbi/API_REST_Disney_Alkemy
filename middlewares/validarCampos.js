20a29f6614ac46a23cbe20211d63fcf4b09b89ce
Bruno Ruiz <ruiz.brunoivan@gmail.com>
2022-02-09 03:48:34 -0200

    Se crea carpeta middlewares junto validacion para express-validation

---
+++ middlewares/validarCampos.js
@@ -0,0 +1,10 @@
+const { validationResult } = require("express-validator");
+
+const validarCampos = (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty())
+        return res.send(errors);
+    next();
+}
+
+module.exports = validarCampos;
