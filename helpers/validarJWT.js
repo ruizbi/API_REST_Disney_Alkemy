3f95f348b62c6a8564adaf6677049d1e30037258
Bruno Ruiz <ruiz.brunoivan@gmail.com>
2022-02-10 01:55:28 -0200

    Se agregan validaciones al jwt para el usuario

--- helpers/validarJWT.js
+++ helpers/validarJWT.js
@@ -1,13 +1,23 @@
 const { request, response } = require("express");
+const UsuarioSchema = require('../models/Usuario');
 require('dotenv').config();
 
-const validarJWT = (req = request, res = response, next) => {
+const validarJWT = async (req = request, res = response, next) => {
   const token = req.header('token');
   if(!token){
     return res.send({message:'No hay token'})
   }
   try {
-    jwt.verify(token, process.env.KEY_JWT);
+    let {_id} = jwt.verify(token, process.env.KEY_JWT);
+    let usuario = await UsuarioSchema.findById({_id});
+    
+    if(!usuario)
+      return res.send({message:'El usuariono existe en la db'});
+
+    if(!usuario.activo)
+      return res.send({message:'El usuario ya no se encuentra activo'});
+
+    req.usuario = usuario;
     next();
   }catch(error) {
     console.log(error);
