6c2cd0556f4c92e6c00aa00d2c84f6e20fbe3122
Bruno Ruiz <ruiz.brunoivan@gmail.com>
2022-02-10 00:30:56 -0200

    Se saca el validarJWT

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const personajeRoutes = require('./routers/personaje_db');
 const peliculaRoutes = require('./routers/pelicula_db');
 const serieRoutes = require('./routers/serie_db');
 const generoRoutes = require('./routers/genero_db');
-const loginRoutes = require('./routers/login');
-const validarJWT = require('./helpers/validarJWT');
+const authRoutes = require('./routers/login');
 const dbConnection = require('./database/config');
 const puertoEscucha = process.env.PUERTO_ESCUCHA || 9001;
 
@@ -15,7 +14,7 @@ app.use("/api", personajeRoutes);
 app.use("/api", peliculaRoutes);
 app.use("/api", serieRoutes);
 app.use("/api", generoRoutes);
-app.use("/auth", loginRoutes);
+app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send({message:'Api catalogo entretenimiento'});
